6e856b775c40c67d569dd2b99650eae8ad520158
JustinHu704 <68578591+JustinHu704@users.noreply.github.com>
2020-12-12 20:26:55 +0800

    Add files via upload

--- guess-num.py
+++ guess-num.py
@@ -1,12 +1,27 @@
 import random
-r = random.randint(1, 100)
+i = 0
+start = input('Please determine the starting value of the random number.: ')
+end = input('Please determine the ending value of the random number.: ')
+end = int(end)
+start = int(start)
+num = random.randint(start, end)
 while True:
-	num = input('Please guess the number: ')
-	num = int(num)
-	if num == r:
-		print('Your guess is correct.')
+	guess = input('Please enter your guess.: ')
+	guess = int(guess)
+	i =+ 1
+	if guess == num:
+		print('Your guession is correct.')
+		if i == 1:
+			print('You use', i, 'time.')
+		else:
+			print('You use', i, 'times.')
 		break
-	elif r > num:
-		print('Your guess is smaller than the answer.')
-	elif r < num:
+	elif guess > num:
 		print('Your guess is larger than the answer.')
+	elif guess < num:
+		print('Your guess is smaller than the answer.')
+	if i == 1:
+		print('You use', i, 'time.')
+	else:
+		print('You use', i, 'times.')
+
